f3eebfe6c92795150ef437a965f52c6aa5f3ab2e
xzz <xzz@xzzs-MacBook-Pro.local>
2021-10-05 15:11:47 -0700

    display box

--- js/activity.js
+++ js/activity.js
@@ -11,6 +11,18 @@ $(document).ready(function() {
     $("tbody tr td").click(function() {
         if (isHighlightable($(this))) {
             $(this).toggleClass("highlighted");
+            
+            if ($(this).hasClass("highlighted")) {
+                $('#displaySelected').css("visibility" ,"visible");
+                $('#displaySelected').css("margin-top", "2em");
+                $('#result').append("<p>" + content + "</p>");
+            } else {
+               $('#result p:contains('+content+')').remove();
+               if ($('#result').has('p').length == false) {
+                   $('#displaySelected').css("visibility", "hidden");
+                   $('#displaySelected').css("margin-top", "0");
+               }
+            }
         }
     })
 });
