e8a17a836981731af516951aa04563ca3b74dbf1
xzz <xzz@xzzs-MacBook-Pro.local>
2021-10-19 22:18:00 -0700

    bootstrap applied

--- js/activity.js
+++ js/activity.js
@@ -9,15 +9,19 @@ $(document).ready(function() {
         }
     })
     $("tbody tr td").click(function() {
+        var content = $(this).text();
+        var colIndex = $(this).index();
+        var colName = $("th:eq("+ colIndex +")").text();
+
         if (isHighlightable($(this))) {
             $(this).toggleClass("highlighted");
             
             if ($(this).hasClass("highlighted")) {
                 $('#displaySelected').css("visibility" ,"visible");
                 $('#displaySelected').css("margin-top", "2em");
-                $('#result').append("<p>" + content + "</p>");
+                $('#result').append("<p>" + content + " at " + colName + "</p>");
             } else {
-               $('#result p:contains('+content+')').remove();
+               $('#result p:contains('+ content +')').remove();
                if ($('#result').has('p').length == false) {
                    $('#displaySelected').css("visibility", "hidden");
                    $('#displaySelected').css("margin-top", "0");
